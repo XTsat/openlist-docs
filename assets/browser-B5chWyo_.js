a589eb0cfedf06df9fa92a2d8b1b4144dff7b347
cxw620 <cxw620@users.noreply.github.com>
2025-06-12 08:16:44 +0000

    deploy: ecd0ec90c871aa5e4d7d79c79476613d1613028a

--- assets/browser-B5chWyo_.js
+++
@@ -1,1 +0,0 @@
-import{i}from"./app-DDGTVc3m.js";const r=i&&"chrome"in window;i&&navigator.userAgent.includes("Firefox");const s=i&&navigator.userAgent.includes("Safari")&&!r;export{s as i};
--- assets/qq-CjlNvnGB.js
+++
@@ -1,1 +0,0 @@
-const s="/docs/img/sponsor/alipay.jpg",o="/docs/img/sponsor/wechat.png",p="/docs/img/sponsor/qq.png";export{p as _,o as a,s as b};
